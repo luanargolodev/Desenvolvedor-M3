6f577e4b4db6997dc1f6ce17af639875d95a17da
Luan Argolo <ipsluan@icloud.com>
2023-04-13 00:05:40 -0300

    feat(index): get products and insert to page

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,49 @@
 import { Product } from "./Product";
+import { formatNumberToMoney } from "./formatNumberToMoney";
 
 const serverUrl = "http://localhost:5000";
 
-function main() {
-  console.log(serverUrl);
-}
+const Default = {
+  init: function () {
+    document.addEventListener("DOMContentLoaded", () => {
+      Default.getProducts();
+    });
+  },
 
-document.addEventListener("DOMContentLoaded", main);
+  getProducts: async () => {
+    const limit = 4;
+
+    const response = await fetch(
+      `${serverUrl}/products?&_limit=${limit}&_page=1`
+    );
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      throw new Error(data.message);
+    }
+
+    const products = data.map((product: Product) => {
+      return `
+        <div class="shelf__product">
+          <img
+            class="shelf__product-image"
+            src="${product.image}"
+            alt="${product.name}"
+          />
+          <h3 class="shelf__product-name">${product.name}</h3>
+          <span class="shelf__product-price">${formatNumberToMoney(
+            product.price
+          )}</span>
+          <span class="shelf__product-installments">até ${
+            product.parcelamento[0]
+          }x de R$ ${product.parcelamento[1]}</span>
+          <button class="shelf__product-buybutton">Comprar</button>
+        </div>`;
+    });
+
+    const $shelf = document.querySelector(".shelf__products")!;
+    $shelf.innerHTML = products.join("");
+  },
+};
+
+Default.init();
