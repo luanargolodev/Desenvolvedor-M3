5bc6520d74b98fadb078308aeb1b30c07d2b2e57
Luan Argolo <ipsluan@icloud.com>
2023-04-13 00:29:39 -0300

    refactor(index): change variables

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -2,28 +2,38 @@ import { Product } from "./Product";
 import { formatNumberToMoney } from "./formatNumberToMoney";
 
 const serverUrl = "http://localhost:5000";
+const limit = 4;
 
-const Default = {
+const Shelf = {
   init: function () {
     document.addEventListener("DOMContentLoaded", () => {
-      Default.getProducts();
+      Shelf.getProducts();
+      Shelf.showMoreProducts();
     });
   },
 
   getProducts: async () => {
-    const limit = 4;
-
     const response = await fetch(
       `${serverUrl}/products?&_limit=${limit}&_page=1`
     );
     const data = await response.json();
 
-    if (response.status !== 200) {
+    if (!response.ok) {
       throw new Error(data.message);
     }
 
-    const products = data.map((product: Product) => {
-      return `
+    const productsHtml = Shelf.createProductsHtml(data);
+
+    const $shelf = document.querySelector(".shelf__products")!;
+    $shelf.innerHTML = productsHtml;
+
+    Shelf.setupBuyProduct();
+  },
+
+  createProductsHtml: (products: Product[]): string => {
+    return products
+      .map((product) => {
+        return `
         <div class="shelf__product">
           <img
             class="shelf__product-image"
@@ -39,11 +49,51 @@ const Default = {
           }x de R$ ${product.parcelamento[1]}</span>
           <button class="shelf__product-buybutton">Comprar</button>
         </div>`;
-    });
+      })
+      .join("");
+  },
 
-    const $shelf = document.querySelector(".shelf__products")!;
-    $shelf.innerHTML = products.join("");
+  setupBuyProduct: () => {
+    const $buttonBuy = document.querySelectorAll(".shelf__product-buybutton");
+    const $numberProductsInCart = document.querySelector(
+      ".header__content__cart-text"
+    )!;
+
+    $buttonBuy.forEach((button) => {
+      button.addEventListener("click", () => {
+        alert("Produto comprado com sucesso!");
+
+        const numberProducts = Number($numberProductsInCart.textContent);
+        $numberProductsInCart.textContent = String(numberProducts + 1);
+      });
+    });
+  },
+
+  showMoreProducts: () => {
+    const $buttonShowMoreProducts =
+      document.querySelector<HTMLButtonElement>(".shelf__show-more")!;
+    let currentPage = 1;
+
+    $buttonShowMoreProducts.addEventListener("click", async () => {
+      const response = await fetch(
+        `${serverUrl}/products?&_limit=${limit}&_page=${currentPage + 1}`
+      );
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.message);
+      }
+
+      const productsHtml = Shelf.createProductsHtml(data);
+      const $shelf = document.querySelector(".shelf__products")!;
+      $shelf.innerHTML += productsHtml;
+
+      currentPage++;
+      $buttonShowMoreProducts.dataset.page = String(currentPage);
+
+      Shelf.setupBuyProduct();
+    });
   },
 };
 
-Default.init();
+Shelf.init();
